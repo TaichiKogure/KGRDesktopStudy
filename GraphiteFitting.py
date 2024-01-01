62e92985d26dc3d97d988fc00d9368120f877484
TK <taichi.kogure1105@gmail.com>
2024-01-01 09:06:10 +0800

    Add aws, csv-editor, other configuration files and GraphiteFitting script
    This commit introduces configuration files for AWS settings, CSV editor, and other misc settings. AWS settings have been duplicated from misc and adapted to their own file. Additionally, a new python script, GraphiteFitting.py, is added which performs spline interpolation on data from a CSV file. Lastly, the CSV file attributes are defined within a new config file.

---
+++ GraphiteFitting.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from scipy.interpolate import UnivariateSpline
+import matplotlib.pyplot as plt
+import numpy as np
+
+# CSVファイルを読み込む
+df = pd.read_csv('LiC6_OCV2.csv')
+
+# XとYのデータ抜き出し
+data = df[['x', 'Potential(V)']].values
+
+# Sort data by x
+data = data[data[:,0].argsort()]
+
+x = data[:, 0]
+y = data[:, 1]
+
+# データをスプライン補間
+
+spline = UnivariateSpline(x, y, s=70)
+
+# 曲線上のデータポイント
+x_range = np.linspace(min(x), max(x), num=1000)
+y_range = spline(x_range)
+
+# プロット
+plt.scatter(x, y, label='Original Data', alpha=0.6)
+plt.plot(x_range, y_range, color='red', label='Spline Interpolation')
+plt.xlabel('x')
+plt.ylabel('Potential(V)')
+plt.title('Spline Interpolation of Data')
+plt.legend()
+plt.show()
